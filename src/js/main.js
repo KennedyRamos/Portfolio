a6681ef5e761017864886551a3f0409f30274b8d
Kennedy Ramos <devnedy@gmail.com>
2025-07-04 22:34:21 -0300

    acréscimo no menu, criação das sections

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,15 @@
-const menu = document.querySelector('.toggle')
-const nav = document.querySelector('.header__navigation')
+const menuBtn = document.querySelector('.toggle');
+const nav = document.querySelector('.header__navigation');
 
-menu.addEventListener('click', () => {
+menuBtn.addEventListener('click', (e) => {
+  e.stopPropagation();
   nav.classList.toggle('active');
+});
+
+document.addEventListener('click', (e) => {
+  const target = e.target;
+
+  if (!nav.contains(target) && !menuBtn.contains(target)) {
+    nav.classList.remove('active');
+  }
 });
